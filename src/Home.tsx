e04697c1ae1af315abe1a504914ff7825896f092
Fergus Lai <ferguslai724@gmail.com>
2025-02-27 14:46:56 +0000

    Fix dark mode bg

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 function Home() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center gap-6 text-neutral-800 dark:text-neutral-200">
+        <div className="flex h-screen w-screen items-center justify-center gap-6 bg-neutral-200 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
             <NavLink
                 to="/Board"
                 className="rounded-3xl p-2 hover:bg-neutral-400 dark:hover:bg-neutral-700"
