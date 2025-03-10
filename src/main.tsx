936389f4e80ff975a3cd6c68d4969fffcabe1262
Fergus Lai <ferguslai724@gmail.com>
2025-03-10 16:35:19 +0000

    Change Route name to Game instead of Board

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="Board">
+                <Route path="Game">
                     <Route index element={<Local />} />
                     <Route path="Multiplayer/:roomID" element={<GameBoard />} />
                 </Route>
