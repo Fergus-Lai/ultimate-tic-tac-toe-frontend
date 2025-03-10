14b1a163e160165883d628377c4ed819b44eefaa
Fergus Lai <ferguslai724@gmail.com>
2025-03-10 22:33:01 +0000

    Handle join game and error

--- src/Multiplayer/Game.tsx
+++ src/Multiplayer/Game.tsx
@@ -1,12 +1,18 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { GameProvider } from "~/board/GameProvider";
 import { GameBoard } from "~/board/GameBoard";
 import { socket } from "~/socket";
+import { Bounce, toast, ToastContainer } from "react-toastify";
+
+interface SocketError {
+    message: string;
+}
 
 export const Multiplayer: React.FC = () => {
     const { roomID } = useParams();
     const [connected, setConnected] = useState(socket.connected);
+    const navigate = useNavigate();
     useEffect(() => {
         const onConnected = () => {
             setConnected(true);
@@ -16,16 +22,39 @@ export const Multiplayer: React.FC = () => {
         };
         const onDisconnected = () => {
             setConnected(false);
+            setTimeout(function () {
+                console.log("I");
+                window.location.replace("/Game/Lobby");
+            }, 5000);
+        };
+
+        const onError = (data: SocketError) => {
+            toast.error(data.message + ", redirecting to lobby", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+            });
+            socket.disconnect();
         };
         socket.on("connect", onConnected);
         socket.on("disconnect", onDisconnected);
+        socket.on("error", onError);
+        socket.connect();
         return () => {
             socket.off("connect", onConnected);
             socket.off("disconnect", onDisconnected);
+            socket.off("error", onError);
+            socket.disconnect();
         };
-    }, [roomID]);
+    }, [roomID, navigate]);
     return (
         <>
+            <ToastContainer />
             {connected ? (
                 <GameProvider roomID={roomID}>
                     <GameBoard />
