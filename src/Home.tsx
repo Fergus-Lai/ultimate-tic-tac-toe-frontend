a7ece4ee514849e9834b2b030f3ba32a37895aaa
Fergus Lai <ferguslai724@gmail.com>
2025-03-16 02:01:02 +0000

    Add Style to Home Page Button and Title

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,16 +2,17 @@ import { NavLink } from "react-router";
 
 function Home() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center gap-6 bg-neutral-200 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-neutral-200 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
+            <div className="text-4xl font-bold">Ultimate Tic Tac Toe</div>
             <NavLink
                 to="/Game"
-                className="rounded-3xl p-2 hover:bg-neutral-400 dark:hover:bg-neutral-700"
+                className="flex w-1/4 items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
             >
                 Local
             </NavLink>
             <NavLink
                 to="/Game/Lobby"
-                className="rounded-3xl p-2 hover:bg-neutral-400 dark:hover:bg-neutral-700"
+                className="flex w-1/4 items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
             >
                 Multiplayer
             </NavLink>
