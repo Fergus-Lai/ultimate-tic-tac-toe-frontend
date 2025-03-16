6b664cef7d32a6744115bab730f96b688d653f28
Fergus Lai <ferguslai724@gmail.com>
2025-03-16 01:58:58 +0000

    Add Home Page Button for Lobby

--- src/Multiplayer/Lobby.tsx
+++ src/Multiplayer/Lobby.tsx
@@ -6,57 +6,65 @@ function Lobby() {
     const [roomID, setRoomID] = useState("");
     const navigate = useNavigate();
     return (
-        <div className="flex h-screen w-screen items-center justify-center gap-6 bg-neutral-200 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
+        <div className="flex h-screen w-screen flex-col items-center justify-center gap-6 bg-neutral-200 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200">
             <ToastContainer />
-            <div className="flex h-1/6 flex-row gap-2">
-                <button
-                    className="flex h-full items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
-                    onClick={async () => {
-                        const res = await fetch(
-                            "http://localhost:3000/create-room",
-                            {
-                                method: "POST",
-                            },
-                        );
-                        const createdRoomID = (await res.json()).roomId;
-                        navigate(`/Game/Multiplayer/${createdRoomID}`);
-                    }}
-                >
-                    Create Game
-                </button>
-                <div className="flex flex-col justify-between gap-4">
-                    <input
-                        value={roomID}
-                        onChange={(e) =>
-                            setRoomID(e.target.value.toUpperCase())
-                        }
-                        type="text"
-                        className="w-full flex-1/2 rounded-3xl border-2 border-neutral-400 text-center"
-                        maxLength={6}
-                    />
-                    <NavLink
-                        to={`/Game/Multiplayer/${roomID}`}
-                        className="flex flex-1/2 items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
-                        aria-disabled={!!roomID.match("[A-Z0-9]{6}")}
-                        onClick={(e) => {
-                            if (!roomID.match("[A-Z0-9]{6}")) {
-                                e.preventDefault();
-                                toast.error("Incorrect Room ID", {
-                                    position: "top-right",
-                                    autoClose: 5000,
-                                    hideProgressBar: false,
-                                    closeOnClick: false,
-                                    pauseOnHover: true,
-                                    progress: undefined,
-                                    theme: "colored",
-                                    transition: Bounce,
-                                });
-                            }
+            <div className="flex h-1/6 flex-col gap-4">
+                <div className="flex h-full flex-row gap-2">
+                    <button
+                        className="flex h-full items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
+                        onClick={async () => {
+                            const res = await fetch(
+                                "http://localhost:3000/create-room",
+                                {
+                                    method: "POST",
+                                },
+                            );
+                            const createdRoomID = (await res.json()).roomId;
+                            navigate(`/Game/Multiplayer/${createdRoomID}`);
                         }}
                     >
-                        Join
-                    </NavLink>
+                        Create Game
+                    </button>
+                    <div className="flex flex-col justify-between gap-4">
+                        <input
+                            value={roomID}
+                            onChange={(e) =>
+                                setRoomID(e.target.value.toUpperCase())
+                            }
+                            type="text"
+                            className="w-full flex-1/2 rounded-3xl border-2 border-neutral-400 text-center"
+                            maxLength={6}
+                        />
+                        <NavLink
+                            to={`/Game/Multiplayer/${roomID}`}
+                            className="flex flex-1/2 items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
+                            aria-disabled={!!roomID.match("[A-Z0-9]{6}")}
+                            onClick={(e) => {
+                                if (!roomID.match("[A-Z0-9]{6}")) {
+                                    e.preventDefault();
+                                    toast.error("Incorrect Room ID", {
+                                        position: "top-right",
+                                        autoClose: 5000,
+                                        hideProgressBar: false,
+                                        closeOnClick: false,
+                                        pauseOnHover: true,
+                                        progress: undefined,
+                                        theme: "colored",
+                                        transition: Bounce,
+                                    });
+                                }
+                            }}
+                        >
+                            Join
+                        </NavLink>
+                    </div>
                 </div>
+                <NavLink
+                    to={"/"}
+                    className="flex items-center justify-center rounded-3xl bg-neutral-700 p-2 hover:bg-neutral-400 dark:hover:bg-neutral-500"
+                >
+                    Home Page
+                </NavLink>
             </div>
         </div>
     );
